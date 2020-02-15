d111cab85302ba41c74c2731d367ad3866a0c35d
Deer404 <919187569@qq.com>
2020-02-15 19:28:08 +0800

    热销推荐组件开发完成

--- travel_new/src/pages/home/components/Icons.vue
+++ travel_new/src/pages/home/components/Icons.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="icons">
 		<swiper>
-			<swiper-slide v-for='(page,index) of pages' :key="index">
+			<swiper-slide v-for="(page, index) of pages" :key="index">
 				<div class="icon" v-for="item of page" :key="item.id">
 					<div class="icon-img"><img class="icon-img-content" :src="item.imgUrl" /></div>
 					<p class="icon-desc">{{ item.desc }}</p>
@@ -65,17 +65,17 @@ export default {
 			]
 		};
 	},
-	computed:{
-		pages () {
-			const pages = []
-			this.iconList.forEach((item,index)=>{
-				const page = Math.floor(index/8)
-				if(!pages[page]){
-					pages[page] = []
+	computed: {
+		pages() {
+			const pages = [];
+			this.iconList.forEach((item, index) => {
+				const page = Math.floor(index / 8);
+				if (!pages[page]) {
+					pages[page] = [];
 				}
-				pages[page].push(item)
-			})
-			return pages
+				pages[page].push(item);
+			});
+			return pages;
 		}
 	}
 };
@@ -84,37 +84,40 @@ export default {
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 @import '~styles/mixins.styl'
+
 .icons >>> .swiper-container
 	height 0
 	// overflow hidden container自带了
 	padding-bottom 50%
-.icon
-	position relative
-	overflow hidden
-	float left
-	width 25%
-	height 0
-	padding-bottom 25%
-	.icon-img
-		position absolute
-		top 0
-		left 0
-		right 0
-		bottom 0.44rem
-		padding 0.1rem
-		box-sizing border-box
-		.icon-img-content
-			display block
-			margin 0 auto
-			height 100%
-	.icon-desc
-		position absolute
-		left 0
-		right 0
-		bottom 0
-		line-height 0.44rem
-		height 0.44rem
-		color $darkTextColor
-		text-align center
-		ellipsis()
+.icons
+	margin-top 0.1rem
+	.icon
+		position relative
+		overflow hidden
+		float left
+		width 25%
+		height 0
+		padding-bottom 25%
+		.icon-img
+			position absolute
+			top 0
+			left 0
+			right 0
+			bottom 0.44rem
+			padding 0.1rem
+			box-sizing border-box
+			.icon-img-content
+				display block
+				margin 0 auto
+				height 100%
+		.icon-desc
+			position absolute
+			left 0
+			right 0
+			bottom 0
+			line-height 0.44rem
+			height 0.44rem
+			color $darkTextColor
+			text-align center
+			ellipsis()
 </style>
