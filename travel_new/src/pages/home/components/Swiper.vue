d11158942cdc9857a40cc18e6171efe1e9863687
Deer404 <919187569@qq.com>
2020-02-16 00:48:04 +0800

    使用了ajax请求

--- travel_new/src/pages/home/components/Swiper.vue
+++ travel_new/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="wrapper">
-		<swiper :options="swiperOption">
-			<swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl" /></swiper-slide>
+		<swiper :options="swiperOption" v-if="showSwiper">
+			<swiper-slide v-for="item of list" :key="item.id"><img class="swiper-img" :src="item.imgUrl" /></swiper-slide>
 			<div class="swiper-pagination" slot="pagination"></div>
 		</swiper>
 	</div>
@@ -14,21 +14,20 @@ export default {
 		return {
 			swiperOption: {
 				pagination: '.swiper-pagination',
-				loop : true,
-				autoplay:5000
-			},
-			swiperList: [
-				{
-					id: '0001',
-					imgUrl: 'http://imgs.qunarzz.com/vc/ad/7c/fe/037d1a400372ff57b0030d1d1c.jpg_92.jpg'
-				},
-				{
-					id: '0002',
-					imgUrl: 'http://imgs.qunarzz.com/vc/24/36/10/3fc94d042ae62de380581d4c98.jpg_92.jpg'
-				}
-			]
+				loop: true,
+				autoplay: 5000
+			}
+			// swiperList : this.list
 		};
 	},
+	props: {
+		list: Array
+	},
+	computed: {
+		showSwiper() {
+			return this.list.length;
+		}
+	}
 };
 </script>
 
--- travel_new/vue.config.js
+++ travel_new/vue.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
-function resolve (dir) {
-    return path.join(__dirname, dir)
+
+function resolve(dir) {
+	return path.join(__dirname, dir)
 }
 module.exports = {
-    lintOnSave: true,
-    chainWebpack: (config)=>{
-        config.resolve.alias
-            .set('@$', resolve('src'))
-            .set('styles',resolve('src/assets/styles'))
-    }
+	lintOnSave: true,
+	chainWebpack: (config) => {
+		config.resolve.alias
+			.set('@$', resolve('src'))
+			.set('styles', resolve('src/assets/styles'))
+	},
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080',
+				changeOrigin: true, // needed for virtual hosted sites
+				ws: true, // proxy websockets
+				pathRewrite: {
+					'^/api': '/mock'
+				}
+			}
+		}
+	},
 }
