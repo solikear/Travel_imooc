0eeedde457a7a162a94adc3dffedbfd077804d5f
Deer404 <919187569@qq.com>
2020-02-15 14:20:46 +0800

    Icon图标完成

--- travel_new/src/pages/home/Home.vue
+++ travel_new/src/pages/home/Home.vue
@@ -2,18 +2,20 @@
 	<div class="home">
 		<home-header></home-header>
 		<home-swiper></home-swiper>
-		<div>jaklsdjk</div>
+		<home-icons></home-icons>
 	</div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue';
 import HomeSwiper from './components/Swiper.vue';
+import HomeIcons from './components/Icons.vue';
 export default {
 	name: 'Home',
 	components: {
 		HomeHeader,
-		HomeSwiper
+		HomeSwiper,
+		HomeIcons
 	}
 };
 </script>
