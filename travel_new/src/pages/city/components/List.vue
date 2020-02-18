93dd991d9cfd587a43f5992aa86fbd206f66306d
Deer404 <919187569@qq.com>
2020-02-18 11:08:47 +0800

    finsh city css

---
+++ travel_new/src/pages/city/components/List.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="list" ref='wrapper'>
+		<div>
+			<div class="area">
+				<div class="title border-topbottom">当前城市</div>
+				<div class="button-list">
+					<div class="button-wrapper"><div class="button">北京</div></div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">热门城市</div>
+				<div class="button-list">
+					<div class="button-wrapper" v-for="item of [1, 2, 3, 4, 5, 6, 7, 8, 9]"><div class="button">北京</div></div>
+				</div>
+			</div>
+			<div class="area" v-for="it of [1, 2, 3, 4, 5, 6]">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list"><div class="item border-bottom" v-for="item of [1, 2, 3, 4, 5, 7, 8, 9, 10]">澳门</div></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'	
+export default {
+	name: 'CityList',
+	mounted() {
+		this.scroll = new BScroll(this.$refs.wrapper)
+	}
+};
+</script>
+
+<style lang="stylus" scoped>
+.border-topbottom
+	&:before
+		border-color #ccc
+	&:after
+		border-color #ccc
+.border-bottom
+	&:before
+		border-color #ccc
+.list
+	position absolute
+	top 1.58rem
+	right 0
+	bottom 0
+	left 0
+	overflow hidden
+	.title
+		line-height 0.54rem
+		background #eee
+		padding-left 0.2rem
+		color #666
+		font-size 0.26rem
+	.button-list
+		overflow hidden
+		padding 0.1rem 0.6rem 0.1rem 0.1rem
+		.button-wrapper
+			float left
+			width 33.33%
+			.button
+				margin 0.1rem
+				padding 0.1rem
+				text-align center
+				border 0.02rem solid #ccc
+				border-radius 0.06rem
+	.item-list
+		line-height 0.76rem
+		padding-left 0.2rem
+</style>
